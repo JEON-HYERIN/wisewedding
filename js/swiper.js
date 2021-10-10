428e0e8110ea81dde6254f86d39b216390e94f05
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-10-10 23:04:27 +0900

    CELEBRITY

--- js/swiper.js
+++ js/swiper.js
@@ -1,8 +1,19 @@
 new Swiper('.main-visual .swiper', {
-  // autoplay: true,
+  autoplay: true,
   loop: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   }
+})
+
+new Swiper('.main-celebrity .swiper', {
+  autoplay: true,
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 40,
+  // pagination: {
+  //   el: '.swiper-pagination',
+  //   clickable: true,
+  // }
 })
