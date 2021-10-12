975599e263fb0762692e99ccf8670f99f89a67f9
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-10-12 13:47:10 +0900

    ee

--- js/swiper.js
+++ js/swiper.js
@@ -16,12 +16,25 @@ new Swiper('.main-visual .swiper', {
 new Swiper('.main-celebrity .swiper', {
   autoplay: true,
   loop: true,
-  slidesPerView: 3,
-  spaceBetween: 40,
+  // slidesPerView: 3,
+  // spaceBetween: 40,
   pagination: {
     el: '.main-celebrity .swiper .swiper-pagination',
     clickable: true,
-  }
+  },
+  breakpoints: {
+    280: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    850.1: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    }
+  },
 })
 
 new Swiper('.main-partner .swiper', {
