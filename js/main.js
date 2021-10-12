70db88ada5f67be9dd46f9be4986e62b7e7200ae
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-10-12 19:52:55 +0900

    ss

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 const header = document.querySelector('#header');
 
 window.addEventListener('scroll', function () {
-  if(window.pageYOffset > 0) {
+  if (window.pageYOffset > 0) {
     header.classList.add('scrolled');
   } else {
     header.classList.remove('scrolled');
   }
+});
+
+
+// ARROW UP
+const arrowUp = document.getElementById('arrow-up');
+const wrapper = document.getElementById('wrapper');
+window.addEventListener('scroll', function () {
+  if (window.pageYOffset > 200) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+});
+
+arrowUp.addEventListener('click', function () {
+  wrapper.scrollIntoView({behavior: 'smooth'});
 });
