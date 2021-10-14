9763495a72f719e774045d2ba969be9b33156bf1
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-10-14 18:07:36 +0900

    nav

--- js/common.js
+++ js/common.js
@@ -4,7 +4,7 @@ $(document).on('click', 'a[href="#"]', function (e) {
   e.preventDefault();
 });
 
-const header = document.querySelector('#header');
+var header = document.querySelector('#header');
 
 window.addEventListener('scroll', function () {
   if (window.pageYOffset > 0) {
@@ -14,10 +14,26 @@ window.addEventListener('scroll', function () {
   }
 });
 
+// toggle-btn, close-btn
+var toggleBtn = header.querySelector('.toggle-btn');
+var closeBtn = header.querySelector('.close-btn');
+var gnbWrapper = header.querySelector('.gnb-wrapper');
+
+toggleBtn.addEventListener('click', function () {
+  gnbWrapper.classList.add('open');
+  toggleBtn.classList.add('invisible');
+  closeBtn.classList.remove('invisible');
+});
+
+closeBtn.addEventListener('click', function () {
+  gnbWrapper.classList.remove('open');
+  toggleBtn.classList.remove('invisible');
+  closeBtn.classList.add('invisible');
+});
 
 // ARROW UP
-const arrowUp = document.getElementById('arrow-up');
-const wrapper = document.getElementById('wrapper');
+var arrowUp = document.getElementById('arrow-up');
+var wrapper = document.getElementById('wrapper');
 window.addEventListener('scroll', function () {
   if (window.pageYOffset > 200) {
     arrowUp.classList.add('visible');
