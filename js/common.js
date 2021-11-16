162f9a7e7ead4c0a7dac8b4b794824cc3bcb0882
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-11-16 19:40:31 +0900

    폴더정리

--- js/common.js
+++ js/common.js
@@ -27,55 +27,6 @@ function disable() {
   window.scrollTo(0, scrollPosition);
 }
 
-// header영역
-var header = document.querySelector('#header');
 
-window.addEventListener('scroll', function () {
-  if (window.pageYOffset > 0) {
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  }
-});
-
-$('#header .header__menu li a').on('click', function () {
-  var index = $('#header .header__menu li').index($(this).parent());
-  $('#header .header__menu li').removeClass('active');
-  $('#header .header__menu li:eq(' + index + ')').addClass('active');
-});
-
-// gnb영역
-var toggleBtn = header.querySelector('.toggle-btn');
-var closeBtn = header.querySelector('.close-btn');
-var gnbWrap = header.querySelector('.gnb-wrap');
-
-toggleBtn.addEventListener('click', function () {
-  gnbWrap.classList.add('open');
-  toggleBtn.classList.add('invisible');
-  closeBtn.classList.remove('invisible');
-  enable();
-});
-
-closeBtn.addEventListener('click', function () {
-  gnbWrap.classList.remove('open');
-  toggleBtn.classList.remove('invisible');
-  closeBtn.classList.add('invisible');
-  disable();
-});
-
-// ARROW UP
-var arrowUp = document.getElementById('arrow-up');
-var wrapper = document.getElementById('wrapper');
-window.addEventListener('scroll', function () {
-  if (window.pageYOffset > 200) {
-    arrowUp.classList.add('visible');
-  } else {
-    arrowUp.classList.remove('visible');
-  }
-});
-
-arrowUp.addEventListener('click', function () {
-  wrapper.scrollIntoView({behavior: 'smooth'});
-});
 
 
